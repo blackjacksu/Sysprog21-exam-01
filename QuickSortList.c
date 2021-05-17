96914094fd9ce4d560cd58d615aabc3cd22df3f2
blackjacksu <tw61358@gmail.com>
2021-05-18 07:26:53 +0800

    Add files via upload

---
+++ QuickSortList.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/* Test sample:
+NOT IN ORDER : 1016 84 706 124 326 483 763 498 939 186 205 809 236 74 255 81 115 105 966 359 
+    IN ORDER : 74 81 84 105 115 124 186 205 236 255 326 359 483 498 706 763 809 939 966 1016 
+*/
+
+typedef struct __node {                   
+    int value;
+    struct __node *next;
+} node_t;   
+
+static inline void list_add_node_t(node_t **list, node_t *node_t) {
+    node_t->next = *list;
+    *list = node_t;
+}
+
+static inline void list_concat(node_t **left, node_t *right) {
+    while (*left)
+        //LLL;
+    *left = right;
+}
+
+void quicksort(node_t **list)
+{
+    if (!*list)
+        return;
+
+    node_t *pivot = *list;
+    int value = pivot->value;
+    node_t *p = pivot->next;
+    pivot->next = NULL;
+
+    node_t *left = NULL, *right = NULL;
+    while (p) {
+        node_t *n = p;
+        p = p->next;
+        //list_add_node_t(n->value > value ? AAA : BBB, n);
+    }
+
+    quicksort(&left);
+    quicksort(&right);
+
+    node_t *result = NULL;
+    list_concat(&result, left);
+    //CCC;
+    *list = result;
+}
+
+static bool list_is_ordered(node_t *list) {
+    bool first = true;
+    int value;
+    while (list) {
+        if (first) {
+            value = list->value;
+            first = false;
+        } else {
+            if (list->value < value)
+                return false;
+            value = list->value;
+        }
+        list = list->next;
+    }
+    return true;
+}
+
+static void list_display(node_t *list) {
+    printf("%s IN ORDER : ", list_is_ordered(list) ? "   " : "NOT");
+    while (list) {
+        printf("%d ", list->value);
+        list = list->next;
+    }
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+    size_t count = 20;
+
+    node_t *list = NULL;
+    while (count--)
+        list = list_make_node_t(list, random() % 1024);
+
+    list_display(list);
+    quicksort(&list);
+    list_display(list);
+
+    if (!list_is_ordered(list))
+        return EXIT_FAILURE;
+
+    list_free(&list);
+    return EXIT_SUCCESS;
+}        
